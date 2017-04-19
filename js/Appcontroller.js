29888ffd1d17b3f3602813789b3be2e38df576df
amrithm98 <amrithchallenger@gmail.com>
2017-04-19 23:38:47 +0530

    URL Change

--- js/Appcontroller.js
+++ js/Appcontroller.js
@@ -1,13 +1,13 @@
 "use strict";
 app.controller('AppCtrl', function ($scope, $http) {
 	$scope.userList=[];
-	$http.get('http://localhost:11000/allUsers').then(function(req,res){
+	$http.get('http://104.131.163.77:11000/allUsers').then(function(req,res){
 		console.log(req);
 		$scope.userList=req.data;
 	});
 	$scope.getEvents=function(user)
 	{	
-		$http.post('http://localhost:11000/getRegisteredEvents',{phone:user.phone}).then(function(req,res){
+		$http.post('http://104.131.163.77:11000/getRegisteredEvents',{phone:user.phone}).then(function(req,res){
 			var events="";
 			for(var i=0;i<req.data.length;i++)
 			{
